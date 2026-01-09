e5583c31f512a62a4a43aa6e428f4296435c2322
tonimatasdev <79017964+TonimatasDEV@users.noreply.github.com>
2026-01-09 18:12:41 +0100

    chore: Improve main message

--- internal/adapters/handlers/main_handler.go
+++ internal/adapters/handlers/main_handler.go
@@ -7,6 +7,22 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
+type MainMessage struct {
+	Msg          string `json:"msg"`
+	Wiki         string `json:"wiki"`
+	License      string `json:"license"`
+	Repository   string `json:"repository"`
+	IssueTracker string `json:"issueTracker"`
+}
+
 func HandleMain(w http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
-	util.SendString(w, http.StatusOK, "Hello world! This is the best api to get countries, states and cities!")
+	mainMessage := MainMessage{
+		Msg:          "Hello World! This is the best api to get countries, states and cities!",
+		Wiki:         "https://github.com/TonimatasDEV/country-state-city-api/wiki",
+		License:      "https://github.com/TonimatasDEV/country-state-city-api/blob/master/LICENSE",
+		Repository:   "https://github.com/TonimatasDEV/country-state-city-api",
+		IssueTracker: "https://github.com/TonimatasDEV/country-state-city-api/issues",
+	}
+
+	util.SendJSON(w, http.StatusOK, mainMessage)
 }
